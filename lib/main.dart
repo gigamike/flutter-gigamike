670533e58c57d3be333d96dd7afca6a9f6884aa8
Mik Galon <michaelgalon@utilihub.io>
2024-02-16 15:30:47 +0800

    force lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:gigamike/screens/home.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // Force portrait
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    // Splash screen
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  runApp(const MaterialApp(
-    title: 'Test Widgets',
-    home: Home(),
-  ));
+    runApp(const MaterialApp(
+      title: 'Test Widgets',
+      home: Home(),
+    ));
+  });
+
+  
+
+  
 }
